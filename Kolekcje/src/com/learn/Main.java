38b8c3d66e3cdfb9e20c883d0094bbf1781c1bfd
ZS10s105 <mateuszkowalski199700@gmail.com>
2022-09-15 09:24:53 +0200

    list of guesses (unique) and on end display list of guessed numbers

--- Kolekcje/src/com/learn/Main.java
+++ Kolekcje/src/com/learn/Main.java
@@ -1,46 +1,56 @@
 package com.learn;
 
-import java.util.HashSet;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        int limit = 10;
-        int nums = 6;
-        int bound = 10;
+	public static void main(String[] args) {
+		int limit = 10;
+		int nums = 6;
+		int bound = 10;
 
-        Set<Integer> numbers = new HashSet<>();
-        Random random = new Random();
-        while (numbers.size() < nums)
-            numbers.add(random.nextInt(bound));
+		Set<Integer> numbers = new HashSet<>();
+		Random random = new Random();
+		while (numbers.size() < nums)
+			numbers.add(random.nextInt(bound));
 
-//      cheats:
-//        for (int number : numbers)
-//            System.out.print(number + ", ");
+//		cheats:
+//		for (int number : numbers)
+//			System.out.print(number + ", ");
 
-//        System.out.println(numbers);
+		System.out.println(numbers);
 
-        System.out.println("\nGuess the numbers: ");
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.print("Guesses left: " + (limit--) + " | Enter a number: ");
-            int guess = scanner.nextInt();
-            if (numbers.contains(guess)) {
-                System.out.println("You guessed a number!");
-                numbers.remove(guess);
-            } else
-                System.out.println("Wrong guess!");
+		System.out.println("\nGuess the numbers: ");
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> guesses = new ArrayList<>();
+		ArrayList<Integer> guessed = new ArrayList<>();
 
-            if (numbers.isEmpty()) {
-                System.out.println("\nYou guessed all numbers! You won!");
-                break;
-            }
-            if (limit <= 0) {
-                System.out.println("\nYou run out of guesses! You lost!");
-                break;
-            }
-        }
-    }
+		while (true) {
+			System.out.print("Guesses left: " + (limit--) + " | Enter a number: ");
+			int guess = scanner.nextInt();
+
+			if (guesses.contains(guess) && guesses.size() > 0) {
+				System.out.println("You already guessed that number!");
+				limit++;
+				continue;
+			}
+			guesses.add(guess);
+
+			if (numbers.contains(guess)) {
+				System.out.println("You guessed a number!");
+				guessed.add(guess);
+				numbers.remove(guess);
+			} else
+				System.out.println("Wrong guess!");
+
+			if (numbers.isEmpty()) {
+				System.out.println("\nYou guessed all numbers! You won!\n" + guessed);
+				break;
+			}
+			if (limit <= 0) {
+				System.out.println("\nYou run out of guesses! You lost!\nBut you guessed " + guessed.size() + " numbers!\n" + guessed);
+				break;
+			}
+			// display  guesses
+		}
+	}
 }
