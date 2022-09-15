6ab627023c70fd9f91c11bc5deaaf65af0e59a93
ZS10s105 <mateuszkowalski199700@gmail.com>
2022-09-15 08:59:04 +0200

    limits of guesses and won/lose

--- Kolekcje/src/com/learn/Main.java
+++ Kolekcje/src/com/learn/Main.java
@@ -7,27 +7,38 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        int limit = 6;
+        int limit = 10;
+        int nums = 6;
+        int bound = 10;
+
         Set<Integer> numbers = new HashSet<>();
         Random random = new Random();
-        while (numbers.size() < limit)
-            numbers.add(random.nextInt(10));
+        while (numbers.size() < nums)
+            numbers.add(random.nextInt(bound));
 
 //      cheats:
 //        for (int number : numbers)
 //            System.out.print(number + ", ");
 
+//        System.out.println(numbers);
 
         System.out.println("\nGuess the numbers: ");
         Scanner scanner = new Scanner(System.in);
         while (true) {
+            System.out.print("Guesses left: " + (limit--) + " | Enter a number: ");
             int guess = scanner.nextInt();
             if (numbers.contains(guess)) {
                 System.out.println("You guessed a number!");
                 numbers.remove(guess);
-            }
+            } else
+                System.out.println("Wrong guess!");
+
             if (numbers.isEmpty()) {
-                System.out.println("You guessed all numbers!");
+                System.out.println("\nYou guessed all numbers! You won!");
+                break;
+            }
+            if (limit <= 0) {
+                System.out.println("\nYou run out of guesses! You lost!");
                 break;
             }
         }
