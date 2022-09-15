6509add996889a632dc000fc628c214e1d92d019
ZS10s105 <mateuszkowalski199700@gmail.com>
2022-09-15 08:46:45 +0200

    guess 6 nums to win!

--- Kolekcje/src/com/learn/Main.java
+++ Kolekcje/src/com/learn/Main.java
@@ -1,7 +1,35 @@
 package com.learn;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int limit = 6;
+        Set<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        while (numbers.size() < limit)
+            numbers.add(random.nextInt(10));
+
+//      cheats:
+//        for (int number : numbers)
+//            System.out.print(number + ", ");
+
+
+        System.out.println("\nGuess the numbers: ");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            int guess = scanner.nextInt();
+            if (numbers.contains(guess)) {
+                System.out.println("You guessed a number!");
+                numbers.remove(guess);
+            }
+            if (numbers.isEmpty()) {
+                System.out.println("You guessed all numbers!");
+                break;
+            }
+        }
     }
 }
