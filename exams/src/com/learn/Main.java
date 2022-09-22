847beb787e8a92f48deaa6a8cf61237a141da60e
ZS10s105 <mateuszkowalski199700@gmail.com>
2022-09-22 09:54:49 +0200

    get out with "out"

--- exams/src/com/learn/Main.java
+++ exams/src/com/learn/Main.java
@@ -6,13 +6,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		int[] random = fill_random(50);
-		System.out.println(Arrays.toString(random));
+		System.out.println("Random: " + Arrays.toString(random));
 
 		Set<Integer> unique = unique_set(random);
-		System.out.println(unique.toString());
+		System.out.println("Unique: " + unique.toString());
 
-		List<Integer> primes = primes_list(unique);
-		System.out.println(primes.toString());
+		List<Integer> primes = primes_list(unique, true);
+		System.out.println("Primes: " + primes.toString());
 
 		int[] sequence = longest_sequence(random);
 		System.out.println("\nLongest sequence: " + sequence.length);
@@ -46,23 +46,43 @@ public class Main {
 	}
 
 	/**
-	 * Display all primes number in Unique List
+	 * Create an Erastotenes sieve of primes up to range
+	 * @param range range of numbers to check
+	 * @return List of prime numbers
+	 */
+	public static List<Integer> erastotenes_list(int range) {
+		boolean[] sieve = new boolean[range + 1];
+		Arrays.fill(sieve, true);
+		sieve[0] = false;
+		sieve[1] = false;
+
+		for (int i = 2; i < sieve.length; i++)
+			if (sieve[i])
+				for (int j = i * i; j < sieve.length; j += i)
+					sieve[j] = false;
+
+		List<Integer> primes = new ArrayList<>();
+		for (int i = 0; i < sieve.length; i++)
+			if (sieve[i])
+				primes.add(i);
+		return primes;
+	}
+
+	/**
+	 * List of prime numbers in set
 	 * @param list List of unique numbers
 	 * @return List of primes
 	 */
-	public static List<Integer> primes_list(Set<Integer> list) {
+	public static List<Integer> primes_list(Set<Integer> list, boolean sort) {
+		List<Integer> sieve = erastotenes_list(50);
 		List<Integer> primes = new ArrayList<>();
-		for (int num : list) {
-			boolean isPrime = true;
-			for (int i = 2; i < num; i++) {
-				if (num % i == 0) {
-					isPrime = false;
-					break;
-				}
+		for (int i : list) {
+			if (sieve.contains(i)) {
+				primes.add(i);
 			}
-			if (isPrime)
-				primes.add(num);
 		}
+		if (sort)
+			Collections.sort(primes);
 		return primes;
 	}
 
@@ -72,6 +92,7 @@ public class Main {
 	 * @return array of the longest sequence
 	 */
 	public static int[] longest_sequence(int[] array) {
+		/*
 		int longest = 0;
 		int longestStart = 0;
 		int current = 1;
@@ -91,5 +112,21 @@ public class Main {
 			back[i-longestStart-1] = array[i];
 		}
 		return back;
+    */
+
+		List<Integer> sequence = new ArrayList<>();
+		sequence.add(1);
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] > array[i - 1]) {
+				sequence.add(sequence.get(i - 1) + 1);
+			} else {
+				sequence.add(1);
+			}
+		}
+
+		int max = Collections.max(sequence);
+		int index = sequence.indexOf(max);
+
+
 	}
 }
